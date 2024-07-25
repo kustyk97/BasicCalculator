c758ff86c8ca41b916bd9a526422bc1d6568e815
Tomasz Kuś <67096796+kustyk97@users.noreply.github.com>
2024-07-25 22:41:10 +0200

    add types to functions

--- scripts/mathOperations.py
+++ scripts/mathOperations.py
@@ -1,12 +1,12 @@
 
-def add(value1, value2):
+def add(value1: float, value2: float) -> float:
     return value1 + value2
 
-def sub(value1, value2):
+def sub(value1: float, value2: float) -> float:
     return value1 - value2
 
-def mul(value1, value2):
+def mul(value1: float, value2: float) -> float:
     return value1*value2
 
-def div(numerator, denominator):
+def div(numerator: float, denominator: float) -> float:
     return numerator/denominator
